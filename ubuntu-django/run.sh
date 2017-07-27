7d37589cf30dd15abfd81d4e4d23d7af767a10b4
FangHao <fanghao9342@gmail.com>
2017-07-28 01:03:33 +0800

    Django dev runserver

---
+++ ubuntu-django/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# install uwsgi and requirements
+pip install uwsgi==2.0.12
+pip install -r requirements.txt
+
+# runserver
+python manage.py runserver 0.0.0.0:8001
+# uwsgi --ini etc/uwsgi.ini
+
